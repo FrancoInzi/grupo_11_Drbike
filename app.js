c68b7b25cee5a112a619784ce4b3f9b5bb2b3d33
FrancoInzi <francoinzirillo92@gmail.com>
2022-12-12 18:17:35 -0300

    instalacion de express path y el servidor

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("hola")
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.listen(3000, () => console.log ('Esto fue exitoso'));
+app.get('/', function(req, res){
+    res.send('Página principal') 
+});
+app.get('/404', function(req, res){
+    res.send("Error página no encontrada")
+});
