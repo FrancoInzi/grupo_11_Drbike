55279143651454da102304c3732efc4a439c5f1d
FrancoInzi <francoinzirillo92@gmail.com>
2023-02-13 21:49:44 -0300

    se modifican las rutas

--- src/controler/mainControler.js
+++ src/controler/mainControler.js
@@ -9,29 +9,34 @@ console.log(publicFolderPath);
 mainControler.use(express.static(publicFolderPath) );
 
 const index = (req,res) => {
-    res.sendFile(path.join(__dirname, './views/index.html'));
+    res.render('index.ejs');
 }
 const login = (req,res) => {
-    res.sendFile(path.join(__dirname, './views/login.html'));
+    res.render('users/login.ejs');
 }
 const register = (req,res) => {
-    res.sendFile(path.join(__dirname, './views/register.html'));
+    res.render('users/register.ejs');
+}
+const profile = (req,res) => {
+    res.render('users/profile.ejs');
 }
 const productDetail = (req,res) => {
-    res.sendFile(path.join(__dirname, './views/productDetail.html'));
+    res.render('product/productDetail.ejs');
 }
 const productCart = (req,res) => {
-    res.sendFile(path.join(__dirname, './views/productCart.html'));
+    res.render('product/productCart.ejs');
 }
 const createProduct = (req,res) => {
-    res.sendFile(path.join(__dirname, './views/createProduct.html'));
+    res.render('product/createProduct.ejs');
 }
 
 module.exports = {
     index,
     login,
     register,
+    profile,
     productDetail,
     productCart,
-    createProduct
+    createProduct,
+    
 }
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,20 +1,22 @@
 const express = require('express');
-const {index, login, register, productDetail, productCart, createProduct} = require('../controler/mainControler')
+const {index, login, register, productDetail, productCart, createProduct, profile} = require('../controler/mainControler')
 
 const routerMain = express.Router();
 
 
 routerMain.get('/', index);
 
-routerMain.get('/login', login );
+routerMain.get('/users/login', login );
 
-routerMain.get('/register', register );
+routerMain.get('/users/register', register );
 
-routerMain.get('/productDetail',  productDetail);
+routerMain.get('/users/profile', profile );
 
-routerMain.get('/productCart',  productCart);
+routerMain.get('/product/productdetail',  productDetail);
 
-routerMain.get('/productCart',  createProduct);
+routerMain.get('/product/productcart',  productCart);
+
+routerMain.get('/product/createproduct',  createProduct);
 
 
 module.exports=routerMain;
